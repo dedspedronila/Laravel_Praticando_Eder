f6f9ac555c87f051d506217d2ba94f04d7bf0423
dedspedronila <andre.pedronila@aluno.ifsp.edu.br>
2026-06-10 01:58:17 -0300

    feat:aula#29, exibindo eventos do usuário

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -77,9 +77,11 @@ class EventController extends Controller
     public function dashboard()
     {
         $user   = auth()->user();
-        $events = $user->events; // <-- O problema está nascendo AQUI
+        $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard', ['events' => $events, 'eventsasparticipant' => $eventsAsParticipant]);
     }
 
     public function destroy($id)
@@ -91,7 +93,13 @@ class EventController extends Controller
 
     public function edit($id)
     {
+        $user = auth()->user();
+
         $event = Event::findOrFail($id);
+
+        if($user->id != $event->user_id){
+            return redirect('/dashboard');
+        }
 
         return view('events.edit', ['event' => $event]);
     }
